7158d7e703973930bc81ac8f9e91d646502ff96f
osteph32 <ostephenson29@gmail.com>
2026-04-14 19:55:45 -0700

    Adding main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*
+main.js
+Creates Phaser game instance and starts all scenes.
+*/
+
+const config = {
+    type: Phaser.AUTO,
+    width: C.WIDTH,
+    height: C.HEIGHT,
+    backgroundColor:  `#000000`,
+    parent: document.body,
+
+    /* No arcade physics. I decided to do all the movement manually */
+
+    scene: [MenuScene, DayScene, NightScene],
+};
+
+const game = new Phaser.Game(config);
